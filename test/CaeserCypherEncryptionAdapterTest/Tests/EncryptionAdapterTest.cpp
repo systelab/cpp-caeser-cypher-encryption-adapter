d1b07add24572df9d9fa8cefe7ca1a5d1d8dc5c1
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-04-19 20:56:25 +0200

    Added unit tests for the library

---
+++ test/CaeserCypherEncryptionAdapterTest/Tests/EncryptionAdapterTest.cpp
@@ -0,0 +1,47 @@
+#include "stdafx.h"
+#include "CaeserCypherEncryptionAdapter/EncryptionAdapter.h"
+
+
+using namespace testing;
+
+namespace systelab { namespace encryption { namespace caeser_cypher { namespace unit_test {
+
+	namespace {
+		struct EncryptionAdapterTestData
+		{
+			std::string key;
+			std::string decryptedString;
+			std::string encryptedString;
+		};
+		
+		std::vector<EncryptionAdapterTestData> testData = {
+			{ "Key", "StringToEncrypt", "TBD" }
+		};
+	}
+
+	class EncryptionAdapterTest : public testing::TestWithParam<EncryptionAdapterTestData>
+	{
+	public:
+		void SetUp()
+		{
+			m_encryptionAdapter = std::make_unique<EncryptionAdapter>();
+		}
+
+	protected:
+		std::unique_ptr<IEncryptionAdapter> m_encryptionAdapter;
+	};
+
+
+	TEST_P(EncryptionAdapterTest, testEncryptStringReturnsExpectedValue)
+	{
+		ASSERT_EQ(GetParam().encryptedString, m_encryptionAdapter->encryptString(GetParam().key, GetParam().decryptedString));
+	}
+
+	TEST_P(EncryptionAdapterTest, testDecryptStringReturnsExpectedValue)
+	{
+		ASSERT_EQ(GetParam().decryptedString, m_encryptionAdapter->decryptString(GetParam().key, GetParam().encryptedString));
+	}
+
+	INSTANTIATE_TEST_CASE_P(Test, EncryptionAdapterTest, testing::ValuesIn(testData));
+	
+}}}}
