24af598081e4c41efdd5da8384cd92144a68d3d7
Joaquim Vila Bou <36737101+joaquimvila@users.noreply.github.com>
2021-10-01 15:14:49 +0200

    Added hash for gtest 1.10.0

--- conanfile.py
+++ conanfile.py
@@ -16,15 +16,17 @@ class CaeserCypherEncryptionAdapterConan(ConanFile):
     exports_sources = "*", "!build-*", "!*.yml", "!*.md", "!*.in", "!ci", "!.gitattributes", "!.gitignore", "!LICENSE"
 
     def requirements(self):
-        self.requires("EncryptionAdapterInterface/1.0.4@systelab/stable")
+        self.requires("EncryptionAdapterInterface/1.0.5@systelab/stable")
 
     def build_requirements(self):
         if self.options.gtest == "1.7.0":
             self.build_requires("gtest/1.7.0@systelab/stable")
         elif self.options.gtest == "1.8.1":
             self.build_requires("gtest/1.8.1")
+        elif self.options.gtest == "1.10.0":
+            self.build_requires("gtest/1.10.0#0c895f60b461f8fee0da53a84d659131")
         else:
-            self.build_requires("gtest/1.10.0")
+            self.build_requires(f"gtest/{self.options.gtest}")
 
     def build(self):
         cmake = CMake(self)
